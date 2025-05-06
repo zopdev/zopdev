6e3e3a9826f125452ec084275ac516ff8818f6fe
Satyam Trivedi <112492501+satyamt05@users.noreply.github.com>
2025-05-06 12:15:24 +0530

    Added Topbar

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -3,12 +3,14 @@ import { ErrorBoundary } from './components/organisms/ErrorBoundary';
 import { Suspense } from 'react';
 import { routes } from './routes';
 import CompleteLoader from '@/components/atom/Loaders/CompleteLoader.jsx';
+import TopBar from '@/components/molecule/TopBar/index.jsx';
 
 function App() {
   return (
     <HashRouter>
       <ErrorBoundary>
         <Suspense fallback={<CompleteLoader />}>
+          <TopBar />
           <Routes>
             {routes.map(({ path, component: Component }) => (
               <Route key={path} path={path} element={<Component />} />
