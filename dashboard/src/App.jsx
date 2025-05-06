f2d3ec987d6aa433ea6bd5e1fc39d462c04d2716
Satyam Trivedi <112492501+satyamt05@users.noreply.github.com>
2025-05-06 17:16:51 +0530

    fixed the topbar

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,20 +6,27 @@ import CompleteLoader from '@/components/atom/Loaders/CompleteLoader.jsx';
 import TopBar from '@/components/molecules/TopBar/index.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-function App() {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <HashRouter>
         <ErrorBoundary>
           <Suspense fallback={<CompleteLoader />}>
-            <TopBar />
-            <Routes>
-              {routes.map(({ path, component: Component }) => (
-                <Route key={path} path={path} element={<Component />} />
-              ))}
-            </Routes>
+            <div className="flex flex-col h-screen">
+              <div className="sticky top-0 z-50 bg-white shadow">
+                <TopBar />
+              </div>
+
+              <div className="flex-1 overflow-y-auto">
+                <Routes>
+                  {routes.map(({ path, component: Component }) => (
+                    <Route key={path} path={path} element={<Component />} />
+                  ))}
+                </Routes>
+              </div>
+            </div>
           </Suspense>
         </ErrorBoundary>
       </HashRouter>
