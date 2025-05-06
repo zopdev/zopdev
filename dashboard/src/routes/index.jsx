3329960d0f4166fb2053225b4ea13ab885e87e3d
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-06 12:19:59 +0530

    removed dummy page

--- dashboard/src/routes/index.jsx
+++ dashboard/src/routes/index.jsx
@@ -3,6 +3,5 @@ import { lazy } from 'react';
 export const routes = [
   { path: '/', component: lazy(() => import('../pages/dashboard.jsx')) },
   { path: '/cloud-setup', component: lazy(() => import('../pages/cloud-setup.jsx')) },
-  { path: '/about', component: lazy(() => import('../pages/about')) },
   { path: '*', component: lazy(() => import('../pages/404')) },
 ];
