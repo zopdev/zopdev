127dd2ac281803ff47fbb5a7c57a1deccbe8c0c1
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-09 11:08:06 +0530

    implemented the cloud audit setup screens

---
+++ dashboard/src/components/organisms/TopBar.jsx
@@ -0,0 +1,173 @@
+import { Fragment, useEffect, useState } from 'react';
+import { Disclosure, Transition, Dialog } from '@headlessui/react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline/index.js';
+import AppLogo from '@/assets/svg/AppLogo.jsx';
+import CloudAccountSVG from '@/assets/svg/CloudAccountSvg.jsx';
+import AppLogoWithText from '../../assets/images/applogoWithText.svg';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export function TopBar() {
+  const location = useLocation();
+  const [tab, setTab] = useState();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  useEffect(() => {
+    let DashboardView = navigation
+      .map(function (ele) {
+        return `/${ele.routeName.split('/')[1]}`;
+      })
+      .indexOf(`/${location.pathname.split('/')[1]}`); // Use location.pathname here
+
+    DashboardView = DashboardView !== -1 ? DashboardView || 0 : -1;
+
+    setTab(DashboardView);
+  }, [location.pathname]); // Add location.pathname to dependency array
+
+  const navigation = [
+    {
+      name: 'Dashboard',
+      routeName: '/',
+      hover: true,
+      selected: true,
+      isLoading: false,
+      icon: CloudAccountSVG,
+    },
+  ];
+
+  const navigate = useNavigate();
+  const handleRedirect = (path) => {
+    navigate(path);
+    setSidebarOpen(false);
+  };
+
+  return (
+    <>
+      <Disclosure as="nav" className="bg-white shadow">
+        {() => (
+          <>
+            <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
+              <div className="flex h-16 justify-between">
+                <div className="flex">
+                  <div className="md:flex hidden flex-shrink-0 items-center">
+                    <Link to={'/'}>
+                      <AppLogo />
+                    </Link>
+                  </div>
+                  <button
+                    type="button"
+                    onClick={() => setSidebarOpen(true)}
+                    className="-m-2.5 p-2.5 text-gray-400 md:hidden"
+                  >
+                    <span className="sr-only">Open sidebar</span>
+                    <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+                  </button>
+                  <div className="flex h-16 shrink-0 items-center mx-2 md:hidden ">
+                    <Link onClick={() => setSidebarOpen(false)} to={'/'}>
+                      <img width={134} height={42} src={AppLogoWithText} alt="App Logo" />
+                    </Link>
+                  </div>
+                  <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                    {navigation.map((item, idx) => (
+                      <Link key={idx} to={`${item?.routeName}`}>
+                        <div
+                          className={classNames(
+                            tab === idx && item?.selected
+                              ? ' border-primary-500  text-gray-900'
+                              : ' border-transparent  text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                            ' inline-flex h-full items-center border-b-2 px-1 pt-1 text-sm font-medium',
+                          )}
+                        >
+                          <span>{item?.name}</span>
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </Disclosure>
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-50 md:hidden" onClose={setSidebarOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity duration-300 ease-linear"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300 ease-linear"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-900/80" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 flex">
+            <Transition.Child
+              as={Fragment}
+              enter="transform transition ease-in-out duration-300 sm:duration-300"
+              enterFrom="-translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform transition ease-in-out duration-300 sm:duration-300"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-full"
+            >
+              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out">
+                <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out">
+                  <button
+                    type="button"
+                    onClick={() => setSidebarOpen(false)}
+                    className="-m-2.5 p-2.5"
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon aria-hidden="true" className="h-6 w-6 text-white" />
+                  </button>
+                </div>
+                <div className="flex grow flex-col gap-y-8 overflow-y-auto bg-white px-6 pb-2  ring-1 ring-white/10">
+                  <div className="flex h-16 shrink-0 items-center mx-2 ">
+                    <Link onClick={() => setSidebarOpen(false)} to={'/'}>
+                      <img width={134} height={42} src={AppLogoWithText} alt="App Logo" />
+                    </Link>
+                  </div>
+                  <nav className="flex flex-1 flex-col">
+                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                      <li>
+                        <ul role="list" className="-mx-2 space-y-4">
+                          {navigation.map((item, idx) => (
+                            <li key={idx}>
+                              <div
+                                onClick={() => handleRedirect(item.routeName)}
+                                className={classNames(
+                                  tab === idx && item?.selected
+                                    ? 'bg-gray-50 text-primary-500'
+                                    : 'text-gray-600 hover:bg-gray-50',
+                                  'group flex gap-x-3 rounded-md p-2 text-sm font-medium leading-6 cursor-pointer',
+                                )}
+                              >
+                                <item.icon
+                                  aria-hidden="true"
+                                  className={`h-6 w-6 shrink-0`}
+                                  color={tab === idx ? 'rgb(6 182 212)' : '#9197B3'}
+                                />
+                                {item.name}
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
+}
+
+export default TopBar;
---
+++ dashboard/src/pages/cloud-setup.jsx
@@ -0,0 +1,163 @@
+import React, { useEffect } from 'react';
+import CreateCloud from '@/components/organisms/CreateCloud.jsx';
+import DynamicFormRadioWithIcon from '@/components/atom/Button/RadioButtonWithIcon/index.jsx';
+import Stepper from '@/components/organisms/Stepper.jsx';
+import { usePostAuditData } from '@/queries/CloudAccount/index.js';
+import { transformResourceAuditPayload } from '@/utils/transformer.js';
+
+const ResourceAudit = ({ data, updateData, setIsComplete }) => {
+  const auditOptions = [
+    // {
+    //   label: 'Stale',
+    //   description: 'Identify the resources that are no longer in use.',
+    //   value: 'Stale',
+    // },
+    {
+      label: 'Overprovision',
+      description: 'Resources that have more capacity than needed',
+      value: 'Overprovision',
+    },
+    // {
+    //   label: 'Security',
+    //   description: 'Resources with potential security issues',
+    //   value: 'Security',
+    // },
+    {
+      label: 'Run All',
+      description: 'Run all types of audits on your resources',
+      value: 'run-all',
+    },
+  ];
+  const handleChange = (newValue) => {
+    const updatedData = { ...data, selectedOption: newValue };
+    updateData(updatedData);
+    validateStep(updatedData);
+  };
+
+  const validateStep = (stepData) => {
+    const isValid = !!stepData?.selectedOption;
+    setIsComplete(isValid);
+  };
+
+  useEffect(() => {
+    if (!data?.selectedOption) {
+      updateData({ ...data, selectedOption: 'run-all' });
+    } else {
+      validateStep(data);
+    }
+  }, [data, updateData, setIsComplete]);
+
+  return (
+    <div className="md:flex xs:space-y-12 space-x-8 min-h-[28rem] mt-10 ml-2">
+      <div className="md:w-[34%] md:m-12 md:mx-14">
+        <h2 className="text-md font-semibold mb-2">Select an Audit Type</h2>
+        <p className="text-secondary-600">
+          Evaluate your cloud environment for security risks, performance bottlenecks, or cost
+          inefficiencies.
+        </p>
+      </div>
+      <div className="md:w-[45%] md:my-14">
+        <DynamicFormRadioWithIcon
+          options={auditOptions}
+          name="resource-audit"
+          value={data?.selectedOption}
+          defaultSelected={data?.selectedOption || 'run-all'}
+          onChange={handleChange}
+          orientation="horizontal"
+        />
+      </div>
+    </div>
+  );
+};
+
+// const ScheduleStep = ({ data, updateData, setIsComplete }) => {
+//   const cronPattern =
+//     /(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|sun|mon|tue|wed|thu|fri|sat|\*) ?){5,7})/;
+//   const [error, setError] = useState('');
+//
+//   const handleChange = (e) => {
+//     const { value } = e.target;
+//     const updatedData = { ...data, cronSchedule: value };
+//     updateData(updatedData);
+//     validateStep(updatedData);
+//   };
+//
+//   const validateStep = (stepData) => {
+//     const value = stepData?.cronSchedule || '';
+//     if (value.trim() === '') {
+//       setError('');
+//       setIsComplete(false);
+//     } else if (!cronPattern.test(value)) {
+//       setError('Invalid Schedule Time');
+//       setIsComplete(false);
+//     } else {
+//       setError('');
+//       setIsComplete(true);
+//     }
+//   };
+//
+//   useEffect(() => {
+//     validateStep(data);
+//   }, [data, setIsComplete]);
+//
+//   return (
+//     <div className="md:flex xs:space-y-12 space-x-8 min-h-[28rem] mt-10 ml-2">
+//       <div className="md:w-[34%] md:m-12 md:mx-14">
+//         <h2 className="text-md font-semibold mb-2">Give Audit Schedule</h2>
+//         <p className="text-secondary-600 mb-2">
+//           Specify how often you want to run the resource audit by entering a time interval.
+//         </p>
+//         <p className="text-sm text-secondary-500">
+//           You can use special formats like @daily, @hourly, or @every 5m to set the frequency.
+//         </p>
+//       </div>
+//
+//       <div className="w-[35%] space-y-2 my-12">
+//         <label className="block text-sm font-medium text-secondary-700 mb-1">Enter Schedule</label>
+//         <input
+//           type="text"
+//           name="cronSchedule"
+//           value={data?.cronSchedule || ''}
+//           onChange={handleChange}
+//           placeholder="* 5 * * *"
+//           className="w-full px-3 py-2 border border-secondary-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary-500"
+//         />
+//         {error && <span className="text-yellow-500 text-sm">{error}</span>}
+//       </div>
+//     </div>
+//   );
+// };
+
+const Audit = () => {
+  const postData = usePostAuditData();
+
+  const handleComplete = (data) => {
+    const selectedOptionEntry = Object.values(data).find((item) => item.selectedOption);
+    const selectedOption = selectedOptionEntry?.selectedOption?.toLowerCase();
+    const transformedData = transformResourceAuditPayload(data);
+    const payload = { transformedData, selectedOption };
+    postData.mutate(payload);
+  };
+  const steps = [
+    {
+      title: 'Cloud Account',
+      component: (props) => <CreateCloud {...props} audit={true} />,
+    },
+
+    {
+      title: 'Resource Audit',
+      component: ResourceAudit,
+    },
+    // {
+    //   title: 'Schedule',
+    //   component: ScheduleStep,
+    // },
+  ];
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 w-full overflow-auto text-left pt-8 ">
+      <Stepper steps={steps} handleComplete={handleComplete} postData={postData} />
+    </div>
+  );
+};
+
+export default Audit;
