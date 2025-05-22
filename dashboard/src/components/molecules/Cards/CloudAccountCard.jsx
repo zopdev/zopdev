831222f7c7d895b402e963a517cadc4f6e19ae51
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-22 15:12:10 +0530

    disabled the card link for aws

--- dashboard/src/components/molecules/Cards/CloudAccountCard.jsx
+++ dashboard/src/components/molecules/Cards/CloudAccountCard.jsx
@@ -3,10 +3,17 @@ import { PROVIDER_ICON_MAPPER } from '@/utils/componentMapper';
 import { Link } from 'react-router-dom';
 
 const CloudAccountCard = ({ item }) => {
+  const isGCP = item?.provider === 'gcp';
+  const CardWrapper = isGCP ? Link : 'div';
+  const wrapperProps = isGCP ? { to: `/cloud-accounts/${item?.id}/resources` } : {};
+
   return (
-    <Link to={`/cloud-accounts/${item?.id}/resources`}>
+    <CardWrapper {...wrapperProps}>
       <div
-        className={`break-words rounded-md  duration-200 ease-in-out px-6 py-5 w-[330px] h-[166px] border border-gray-200  hover:bg-gray-50 hover:shadow-lg transition-shadow cursor-pointer`}
+        className={`break-words rounded-md px-6 py-5 w-[330px] h-[166px] border border-gray-200
+          duration-200 ease-in-out
+          ${isGCP ? 'hover:bg-gray-50 hover:shadow-lg cursor-pointer transition-shadow' : ''}
+        `}
       >
         <div className="flex items-center gap-2 mb-5">
           {PROVIDER_ICON_MAPPER?.[item?.provider]}
@@ -17,11 +24,11 @@ const CloudAccountCard = ({ item }) => {
           <span className="text-sm text-gray-500">{item?.providerId}</span>
         </div>
 
-        <div className="text-xs text-gray-400 ">
+        <div className="text-xs text-gray-400">
           Updated At <span className="text-xs text-gray-500">{formatTime(item?.updatedAt)}</span>
         </div>
       </div>
-    </Link>
+    </CardWrapper>
   );
 };
 
