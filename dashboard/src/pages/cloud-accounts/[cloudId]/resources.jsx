3eae1a20ef541d2c16fb0ef32cb6e267f0e03b39
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-22 12:23:32 +0530

    integrated the resource start/suspend api

--- dashboard/src/pages/cloud-accounts/[cloudId]/resources.jsx
+++ dashboard/src/pages/cloud-accounts/[cloudId]/resources.jsx
@@ -4,32 +4,44 @@ import PageHeading from '@/components/atom/PageHeading';
 import SwitchButton from '@/components/atom/Switch';
 import BreadCrumb from '@/components/molecules/BreadCrumb';
 import Table from '@/components/molecules/Table';
-import { useGetCloudResources } from '@/queries/cloud-resources';
+import { toast } from '@/components/molecules/Toast';
+import { useGetCloudResources, usePostResourceState } from '@/queries/cloud-resources';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const headers = [
   { key: 'name', label: 'Name', align: 'left', width: '200px' },
-  { key: 'schedule', label: 'Schedule', align: 'left', width: '250px' },
   { key: 'state', label: 'State', align: 'left', width: '150px' },
   { key: 'instance_type', label: 'Instance Type', align: 'left', width: '120px' },
   { key: 'region', label: 'Region', align: 'left', width: '120px' },
 ];
 
 const CloudResourceRow = (resource) => {
+  const { cloudId } = useParams();
+  const resourceStateChanger = usePostResourceState();
+
   const handleToggle = (state) => {
-    console.log(state);
+    resourceStateChanger.mutate({
+      cloudAccId: parseInt(cloudId),
+      name: resource?.instance_name,
+      type: resource?.instance_type,
+      state: state ? 'START' : 'SUSPEND',
+    });
   };
+
+  useEffect(() => {
+    if (resourceStateChanger?.isError) toast.failed(resourceStateChanger.error?.message);
+  }, [resourceStateChanger]);
 
   return {
     id: resource?.instance_name,
     name: resource?.instance_name,
-    schedule: 'schedule',
     state: (
       <div className="min-w-36">
         <SwitchButton
           labelPosition="right"
-          isEnabled={false}
-          disabled={false}
+          isEnabled={resource?.status === 'RUNNING' ? true : false}
+          disabled={resourceStateChanger?.isPending}
           onChange={handleToggle}
           titleList={{ true: 'Running', false: 'Suspended' }}
           name="status"
@@ -46,10 +58,6 @@ const CloudResourcesPage = () => {
   const cloudResources = useGetCloudResources(cloudId);
 
   const data = cloudResources?.data?.data || [];
-
-  const handleRowClick = (row) => {
-    console.log('Row clicked:', row);
-  };
 
   const breadcrumbList = [
     { name: 'Cloud Accounts', link: '/cloud-accounts' },
@@ -69,7 +77,6 @@ const CloudResourcesPage = () => {
         <Table
           headers={headers}
           data={data}
-          handleRowClick={handleRowClick}
           enableRowClick={false}
           renderRow={CloudResourceRow}
           emptyStateTitle="No Resources Found"
--- dashboard/src/queries/cloud-resources/index.js
+++ dashboard/src/queries/cloud-resources/index.js
@@ -1,13 +1,30 @@
-import { fetchData } from '@/services/api.js';
-import { useQuery } from '@tanstack/react-query';
+import { fetchData, postData } from '@/services/api.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 export function useGetCloudResources(id, options = {}) {
   return useQuery({
     queryKey: ['cloudResourcesGetData', id],
     queryFn: async () => {
-      const url = `/cloud-account/${id}/resources`;
+      const url = `/resources?cloudAccId=${id}`;
       return await fetchData(url);
     },
     ...options,
+  });
+}
+
+export function usePostResourceState() {
+  // const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (payload) => {
+      const response = await postData('/resources/state', payload);
+      return response;
+    },
+    // onSuccess: (_data, variables) => {
+    //   // Invalidate or refetch any related queries if needed
+    //   queryClient.invalidateQueries({
+    //     queryKey: ['resourceState', { name: variables?.name }],
+    //   });
+    // },
   });
 }
