a1643f7684569115d8c0d9fcbd1154a32584a0c5
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2025-05-22 16:00:36 +0530

    implemented the resource toggle functionality to turn-on/trun-off the resources

---
+++ dashboard/src/components/layouts/SidebarLayout.jsx
@@ -0,0 +1,29 @@
+import { Outlet, useParams } from 'react-router-dom';
+import Sidebar from '@/components/organisms/Sidebar';
+import TopBar from '@/components/organisms/TopBar';
+import ResourceIcon from '@/assets/svg/sidebar/ResourceIcon';
+
+export default function SidebarLayout() {
+  const { cloudId } = useParams();
+  return (
+    <div>
+      <TopBar />
+      <div className=" xs:inline-block md:flex h-[90vh] w-[100vw] overflow-hidden ">
+        <div>
+          <Sidebar
+            menu={[
+              {
+                link: `cloud-accounts/${cloudId}/resources`,
+                text: 'Resources',
+                icon: ResourceIcon,
+              },
+            ]}
+          />
+        </div>
+        <main className="px-4 sm:px-6 lg:px-8 w-full overflow-auto text-left pt-8">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
